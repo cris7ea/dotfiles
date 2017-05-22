218bdbb0f6d2509c4a74f9352fe44fcd344d5c14
Cristian Cristea <cristeageorgecristian@gmail.com>
2017-05-22 10:56:35 +0300

    Added new prog to brew, modified vimrc, and others.

--- scripts/macOSDefaults.sh
+++ scripts/macOSDefaults.sh
@@ -30,7 +30,7 @@ defaults write com.apple.universalaccess reduceTransparency -bool true
 defaults write NSGlobalDomain NSUseAnimatedFocusRing -bool false
 
 # Disable Notification Center and remove the menu bar icon
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+# launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
 # Disable smart quotes as they’re annoying when typing code
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
