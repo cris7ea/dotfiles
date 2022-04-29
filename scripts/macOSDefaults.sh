309f1d8a4b7fa5e653bd0ea78b2688cc69512eab
Cristea Cristian <cristeageorgecristian@gmail.com>
2022-04-29 09:29:40 +0200

    Key repeat cmd

--- scripts/macOSDefaults.sh
+++ scripts/macOSDefaults.sh
@@ -3,6 +3,18 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+# Key repeat
+
+# Set a blazingly fast keyboard repeat rate
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
+# defaults write NSGlobalDomain KeyRepeat -int 2
+# defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# End Key repeat
+
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
@@ -53,13 +65,6 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
-
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
